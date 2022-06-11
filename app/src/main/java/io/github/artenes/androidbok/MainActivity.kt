ee4addb9c134964f9a8613da0e08444c43398241
Artenes Nogueira <artenesama@gmail.com>
2022-06-11 10:49:38 -0400

    Initial commit

---
+++ app/src/main/java/io/github/artenes/androidbok/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.artenes.androidbok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.artenes.androidbok.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/io/github/artenes/androidbok/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.artenes.androidbok.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
